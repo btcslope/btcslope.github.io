829d110049c6b96f3632fb78b8d195a6f90d26db
btcslope <70568125+btcslope@users.noreply.github.com>
2020-11-01 23:13:38 +0400

    Update fiat.js

--- assets/js/fiat.js
+++ assets/js/fiat.js
@@ -1,10 +1,14 @@
+$(window).resize(function() {
+    var top_nav_height = $("#main-navbar").height();
+});
+
     $(document).ready(function () {
         $('#currency-ranking').DataTable({
             "paging": false,
             "info": false,
             fixedHeader: {
                 header: true,
-                headerOffset: $('#main-navbar').height()
+                headerOffset: top_nav_height
             },
             responsive: true
         });
